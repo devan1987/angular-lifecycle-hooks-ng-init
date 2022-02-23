c387e4006f294915077fb8794026aa27e40adecc
devan1987 <devan1987@gmail.com>
2022-02-23 15:57:24 +0530

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,18 +1,43 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnDestroy,
+  OnChanges,
+  DoCheck,
+} from '@angular/core';
 
 @Component({
   selector: 'hello',
   template: `<h1>Hello {{name}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`],
 })
-export class HelloComponent implements OnInit {
+export class HelloComponent implements OnInit, OnDestroy, OnChanges, DoCheck {
   @Input() name: string;
+  timeoutInstance = null;
 
-  constructor() {
-    
+  constructor() {}
+
+  ngOnChanges() {
+    console.log('on change');
+  }
+
+  ngDoCheck() {
+    console.log('Do checking');
   }
 
   ngOnInit() {
     console.log('component initialized');
+
+    this.timeoutInstance = setInterval(() => {
+      console.log(new Date());
+    }, 1000);
+  }
+
+  ngOnDestroy() {
+    console.log('component destroyed');
+    if (this.timeoutInstance) {
+      clearInterval(this.timeoutInstance);
+    }
   }
 }
